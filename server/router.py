5514b708e0a1156ac1af8a60b50a77308574074a
Luab <b.maksudov@innopolis.ru>
2025-03-18 19:57:55 +0000

    Formatting

--- server/router.py
+++ server/router.py
@@ -1,13 +1,17 @@
 import orthanc
 import requests
 
+
 def OnChange(changeType, level, resourceId):
     if changeType == orthanc.ChangeType.STABLE_STUDY:
-        print('Stable study: %s' % resourceId)
-        request = {"Resources" : [resourceId]}
+        print("Stable study: %s" % resourceId)
+        request = {"Resources": [resourceId]}
         print(request)
-        requests.post(url="http://127.0.0.1:8042/dicom-web/servers/ai/stow/",json=request)
+        requests.post(
+            url="http://localhost:8042/dicom-web/servers/ai/stow/", json=request
+        )
 
-        #orthanc.RestApiPost('/modalities/ai/store', resourceId)
+        # orthanc.RestApiPost('/modalities/ai/store', resourceId)
+
 
 orthanc.RegisterOnChangeCallback(OnChange)
